f01e2099ca1bb34064123fc3fba814312c0f27e1
rebahaider <rebahaiderema@gmail.com>
2024-05-13 02:48:58 +0600

    secure mongo db user name and password

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -9,12 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-// foodService
-// ilkF0KBXTtaWdF2S
-
-
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.aauiduw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aauiduw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
