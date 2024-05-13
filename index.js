6ef2f944ff3918d8e64b27d61f9dbec07cc06858
rebahaider <rebahaiderema@gmail.com>
2024-05-13 15:03:41 +0600

    use get method to get data from database

--- index.js
+++ index.js
@@ -25,12 +25,22 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const foodCollection = client.db('foodService').collection('foodItems');
+
+        app.get('/foodItems', async (req, res) => {
+            const cursor = foodCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
